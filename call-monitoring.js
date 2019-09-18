8eecfbc67021b604b428789f5510fcb1f5479b49
Tyler Long <tyler4long@gmail.com>
2019-09-18 10:31:57 -0700

    Get rid of ringcentral-js-concise

--- call-monitoring.js
+++ call-monitoring.js
@@ -1,28 +1,36 @@
-import RingCentral from 'ringcentral-js-concise'
-import PubNub from 'ringcentral-js-concise/dist/pubnub'
+import RingCentral from '@ringcentral/sdk'
+import { Subscriptions } from '@ringcentral/subscriptions'
 import fs from 'fs'
 
 const deviceId = fs.readFileSync('temp.txt', 'utf-8')
 
-const rc = new RingCentral(
-  process.env.RINGCENTRAL_CLIENT_ID,
-  process.env.RINGCENTRAL_CLIENT_SECRET,
-  process.env.RINGCENTRAL_SERVER_URL
-)
+const rc = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL,
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET
+})
 
 ;(async () => {
-  await rc.authorize({
+  await rc.login({
     username: process.env.RINGCENTRAL_USERNAME,
     extension: process.env.RINGCENTRAL_EXTENSION,
     password: process.env.RINGCENTRAL_PASSWORD
   })
 
   const r = await rc.get('/restapi/v1.0/account/~/extension')
-  const agentExt = r.data.records.filter(ext => ext.extensionNumber === process.env.RINGCENTRAL_AGENT_EXT)[0]
+  const json = await r.json()
+  const agentExt = json.records.filter(ext => ext.extensionNumber === process.env.RINGCENTRAL_AGENT_EXT)[0]
   console.log(agentExt)
 
-  const pubnub = new PubNub(rc, [`/restapi/v1.0/account/~/extension/${agentExt.id}/telephony/sessions`], async message => {
-    // console.log(JSON.stringify(message, null, 2))
+  const subscriptions = new Subscriptions({
+    sdk: rc
+  })
+  const subscription = subscriptions.createSubscription({
+    pollInterval: 10 * 1000,
+    renewHandicapMs: 2 * 60 * 1000
+  })
+  subscription.setEventFilters([`/restapi/v1.0/account/~/extension/${agentExt.id}/telephony/sessions`])
+  subscription.on(subscription.events.notification, async function (message) {
     if (message.body.parties.some(p => p.status.code === 'Answered' && p.direction === 'Inbound')) {
       const r = await rc.post(`/restapi/v1.0/account/~/telephony/sessions/${message.body.telephonySessionId}/supervise`, {
         mode: 'Listen',
@@ -32,5 +40,5 @@ const rc = new RingCentral(
       console.log(r.data)
     }
   })
-  await pubnub.subscribe()
+  await subscription.register()
 })()
